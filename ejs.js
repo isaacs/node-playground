357c887fb3636833fc9ba106d6430c892b20a999
isaacs <i@foohack.com>
2009-12-27 21:12:15 -0800

    It's working like a dream now.

--- ejs.js
+++ ejs.js
@@ -2,12 +2,10 @@
 var posix = require("posix"),
   path = require("path");
 
-// the goal:
-// support <% code %> and <%=text%>
-// without changing the line-number of the file.
 exports.parse = function ejs_parse (filename) {
   var cacheKey = filename+"___",
     safeName = path.filename(filename).replace(/[^a-zA-Z0-9_]/g, '_');
+
   if (exports.useCache && exports.cache.hasOwnProperty(cacheKey)) {
     return exports.cache[cacheKey];
   }
@@ -15,52 +13,142 @@ exports.parse = function ejs_parse (filename) {
   // todo: this wait() smells funny.  clean it up.
   var src = posix.cat(filename).wait();
   
-  src = '(function parsed_'+safeName+' (result, require, __filename, module) {'+
-    'function print () {result.push.apply(result, arguments)};'+
-    'print(\'' +(
-    src
-      .replace(/[\r\t]/g, ' ')
-      .split("<%").join("\t")
-      .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-      .replace(/\t=(.*?)%>/g, "',$1,'")
-      .split("\t").join("');")
-      .split("%>").join("\n;print('")
-      .split("\r").join("\\'")
-      .split("\n;print('\n\n").join("/*FOOMP*/\n;print('\\n")
-    ) + "');\nreturn result;})";
+  var code = parse(src, filename);
   
-  // now walk through and escape the in-string \n's, because JS is fucking obnoxious.
-  var ESC = "\\",
-    CR = "\n",
-    STR = "'",
-    STR2 = '"',
-    escaped = false,
-    state = null; // null = "not in a string"
-  for (var i = src.indexOf(';print('), l = src.length; i < l; i ++) {
-    var c = src[i];
-    if (escaped) escaped = false;
-    else if (c === ESC) escaped = true;
-    else if (c === state && (state === STR || state === STR2)) state = null;
-    else if (state === null && (c === STR || c === STR2)) state = c;
-    else if (c === CR && (state === STR || state === STR2)) {
-      // splice in the backslash in front of the \n, then step over the new char
-      src = src.split("");
-      src.splice(i, 0, ["\\n\\"]);
-      src = src.join("");
-      i += "\\n\\".length;
-    }
-  }
-  // var fn = process.compile(src, filename);
-  return src;
+  return process.compile(code, filename);
 };
 
 exports.useCache = true;
 exports.cache = {};
 
-var filename = require("path").join(__filename, "../test.ejs");
-var src = exports.parse(filename);
-process.stdio.writeError(src);
-var fn = process.compile(src, filename);
-var out = [];
-fn(out, require, __filename, module);
-process.stdio.write("\n\n\n" + out.join(""));
+function parse (src, filename) {
+  var safeName = require("path").filename(filename).replace(/[^a-zA-Z0-9_]/g, '_'),
+    
+    STATE_JS = "STATE_JS",
+    STATE_JS_CODE = "STATE_JS_CODE",
+    STATE_JS_PRINT = "STATE_JS_PRINT",
+    STATE_JS_PRINT_START = "STATE_JS_PRINT_START",
+    STATE_HTML = "STATE_HTML",
+    
+    STATE_NO_STR = "STATE_NO_STR",
+    STATE_STR1 = "STATE_STR1",
+    STATE_STR2 = "STATE_STR2",
+    
+    STATE_NO_REGEX = "STATE_NO_REGEX",
+    STATE_REGEX = "STATE_REGEX",
+    
+    escaped = false,
+    state = STATE_HTML,
+    str_state = STATE_NO_STR,
+    reg_state = STATE_NO_REGEX,
+    
+    CHR_STR1 = "'",
+    CHR_STR2 = '"',
+    CHR_OPEN = '<',
+    CHR_PCT = '%',
+    CHR_CLOSE = '>',
+    CHR_EQ = '=',
+    CHR_ESC = '\\',
+    STR_OPEN = "<%",
+    STR_PRINT = "<%=",
+    STR_CLOSE = "%>",
+    
+    // the marker of where the STATE_HTML started.
+    // that way, we can replace the whole bit with a varname, and then
+    // stringify all the raw strings to vars at the beginning.
+    marker = 0,
+    
+    // put enough \n's in front of the raw strings so that the line numbers are consistent.
+    cr_count = 0,
+    
+    // The buffer that stores the HTML when we're in STATE_HTML
+    html_buffer = [];
+    
+    // just split, since arrays are more better for this.
+    // we're going to end up with an array of output, and then JSON.stringify()
+    // it at the end.
+    arr = src.split(""),
+    
+    // the output buffer, where the code goes as it's parsed.
+    out = ["(function parsed_", safeName, " (print) {"];
+  
+  for ( var i = 0, l = arr.length; i < l; i ++ ) {
+    var c = arr[i];
+    if (state === STATE_HTML) {
+      // check to see if it's time to switch out.
+      if (c === CHR_PCT && html_buffer[html_buffer.length - 1] === CHR_OPEN) {
+        state = STATE_JS;
+        // pop the '<' off the html_buffer, since we don't want to print that
+        html_buffer.pop();
+        // push the print statement onto the output buffer
+        while (cr_count -- > 0) out.push("\n");
+        cr_count = 0;
+        if (html_buffer.length) {
+          out.push(';print(', JSON.stringify(html_buffer.join("")), ');');
+        }
+        html_buffer = [];
+      } else {
+        if (c === "\n") {
+          cr_count ++;
+        }
+        html_buffer.push(c);
+      }
+    } else if ( state === STATE_JS ) {
+      // figure out if we want code or print.
+      if (c === CHR_EQ) {
+        state = STATE_JS_PRINT_START;
+      } else {
+        state = STATE_JS_CODE;
+      }
+      // back up a char so that we can process this.
+      // that's so that we don't bork on <%%> 
+      i --;
+    } else if (state === STATE_JS_PRINT_START) {
+      // push the print statement onto the buffer.
+      out.push("print(");
+      state = STATE_JS_PRINT;
+    } else {
+      // now it gets interesting.
+      // we basically just need to watch for a %> that *isn't*
+      // in a string or regex.  When that happens, end the print() statement
+      // if we're in STATE_JS_PRINT, and flip into STATE_HTML.
+      if (str_state === STATE_STR1) {
+        if (c === CHR_ESC || escaped) {
+          escaped = !escaped;
+        } else if ( c === CHR_STR1 ) {
+          str_state = STATE_NO_STR;
+        }
+        out.push(c);
+      } else if (str_state === STATE_STR2) {
+        if (c === CHR_ESC || escaped) {
+          escaped = !escaped;
+        } else if ( c === CHR_STR2 ) {
+          str_state = STATE_NO_STR;
+        }
+        out.push(c);
+      } else if (reg_state === STATE_REGEX) {
+        if (c === CHR_ESC || escaped) {
+          escaped = !escaped;
+        } else if ( c === CHR_REG ) {
+          reg_state = STATE_NO_REGEX;
+        }
+        out.push(c);
+      } else {
+        // not in a string or regexp.
+        if (c === CHR_CLOSE && out[out.length - 1] === CHR_PCT) {
+          out.pop();
+          if (state === STATE_JS_PRINT) {
+            out.push(');');
+          }
+          state = STATE_HTML;
+          str_state = STATE_NO_STR;
+          reg_state = STATE_NO_REGEX;
+        } else {
+          out.push(c);
+        }
+      }
+    }
+  } // end for
+  out.push('})');
+  return out.join("");
+};
--- test.ejs.js
+++ test.ejs.js
@@ -1,24 +1,27 @@
-(function parsed_test_ejs (result, require, __filename, module) {function print () {result.push.apply(result, arguments)};print('<h1>hello</h1>\n\
-\n\
-"can\'t get no satisfaction"\n\
-\n\
-',"world",'\n\
-\n\
-', new Error("testing").stack ,'\n\
-\n\
-'); for (var i = 0; i < 5; i ++) { 
-;print('\n\
- ', +(new Error("iterating").stack.split("\n")[1].split(":")[1]) === 10 ,'\n\
- '); // new Error("iterating").stack.split("\n")[1] 
-;print('\n\
- ', +(new Error("iterating").stack.split("\n")[1].split(":")[1]) === 12 ,'\n\
-'); } /*FOOMP*/
-;print('\n');
+(function parsed_test_ejs (print) {
+
+
+
+;print("<h1>hello</h1>\n\n\"can't get no satisfaction\"\n\n");print("world");
+
+;print("\n\n");print( new Error("testing").stack );
+
+;print("\n\n"); for (var i = 0; i < 5; i ++) {
+	
+	
+	
+	
+	
+;print("\n\t");print( +(new Error("iterating").stack.split("\n")[1].split(":")[1]) === 15 );
+;print("\n\t"); // new Error("iterating").stack.split("\n")[1]
+	
+;print("\n\t");print( +(new Error("iterating").stack.split("\n")[1].split(":")[1]) === 18 );
+;print("\n"); } 
+
+;print("\n\n");
 if (Math.floor(Math.random() * 2) === 1) {
- print("hello")
+	print("hello")
 } else {
- print("goodbye");
+	print("goodbye");
 }
-/*FOOMP*/
-;print('\n<h2>let\'s see if it worked, neh?</h2>');
-return result;})
+})
