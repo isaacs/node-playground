8541e34f9b091ac1f793ce8194a925eec5fe4088
isaacs <i@foohack.com>
2010-01-10 21:34:20 -0800

    Put the function bodies back.

--- with-bench/with.js
+++ with-bench/with.js
@@ -8,21 +8,21 @@ var outside = "outside";
 
 // (function (foo) {
 with ({foo:"bar"}) {
-//   // set a global
-//   bar = process.version;
-//   // set the local
-//   foo = process.ARGV;
-//   // read the local
-//   var f = foo;
-//   // read the wrapper arg.
-//   var asdf = wrapperArg;
-//   // read a global
-//   var p = process;
-//   // read the var from outside this wrapper
-//   var o = outside;
-//   // set the outside var from inside the wrapper
-//   outside = foo;
-// // })("bar");
+  // set a global
+  bar = process.version;
+  // set the local
+  foo = process.ARGV;
+  // read the local
+  var f = foo;
+  // read the wrapper arg.
+  var asdf = wrapperArg;
+  // read a global
+  var p = process;
+  // read the var from outside this wrapper
+  var o = outside;
+  // set the outside var from inside the wrapper
+  outside = foo;
+// })("bar");
 }
 
 // assert.equal(GLOBAL.bar, process.version);
