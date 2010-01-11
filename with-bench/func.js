91b30c2fa9745295c5f1e9dbf2b30edba06dd776
isaacs <i@foohack.com>
2010-01-10 21:43:45 -0800

    Move tests around to better model use-case.
    The with(){} was being evaluated on each run, rather than just once and have the function defined within it.  In the case under consideration, a with(){} will be evaluated on each module load, and then the module functions will be defined within it.
    
    Thus, the purpose of the test is not to measure how much time interpreting a with(){} adds to execution, but rather the effect on the resulting code of having the extra object in the lookup chain.

--- with-bench/func.js
+++ with-bench/func.js
@@ -1,34 +1,23 @@
-
-var assert = require("assert");
-
-exports.test = function testFunc () {
-
-(function (wrapperArg) {
 var outside = "outside";
-
 (function (foo) {
 // with ({foo:"bar"}) {
-  // set a global
-  bar = process.version;
-  // set the local
-  foo = process.ARGV;
-  // read the local
-  var f = foo;
-  // read the wrapper arg.
-  var asdf = wrapperArg;
-  // read a global
-  var p = process;
-  // read the var from outside this wrapper
-  var o = outside;
-  // set the outside var from inside the wrapper
-  outside = foo;
+  exports.test = function testWith () {
+    (function (wrapperArg) {
+      // set a global
+      bar = process.version;
+      // set the local
+      foo = process.ARGV;
+      // read the local
+      var f = foo;
+      // read the wrapper arg.
+      var asdf = wrapperArg;
+      // read a global
+      var p = process;
+      // read the var from outside this wrapper
+      var o = outside;
+      // set the outside var from inside the wrapper
+      outside = foo;
+    })("asdf");
+  };
 })("bar");
 // }
-
-// assert.equal(GLOBAL.bar, process.version);
-// assert.equal(GLOBAL.foo, undefined);
-// assert.equal(outside, process.ARGV);
-
-})("asdf");
-
-};
