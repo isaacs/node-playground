837ef25d68f781e17276d96690fbeee0516d8aaa
isaacs <i@izs.me>
2010-01-29 16:15:33 -0800

    A benchmark script for comparing various ways to join an array of strings together.

---
+++ array-concat-bench.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node-bench
+
+var list = [
+  "foo",
+  "bar",
+  "baz",
+  "quux",
+  "asdf",
+  "qwerty",
+  "bonk",
+  "whack",
+  "zoom",
+  "zap"
+];
+
+exports.compare = {
+  join : function () {
+    var str = list.join("");
+  },
+  concat : function () {
+    var str = ("").concat(list);
+  },
+  strplus : function () {
+    var str = "";
+    for (var i = 0, l = list.length; i < l; i ++) str += list[i];
+  }
+}
